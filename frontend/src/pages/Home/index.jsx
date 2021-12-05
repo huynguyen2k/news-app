d72f8f2ce4a632661bc0bab0da9de6229e79b2ec
Huy Nguyen <huynguyen235023@gmail.com>
2021-12-05 10:29:57 +0700

    Add pagination to home page

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import './style.scss'
 // HOOKS
 import { useDispatch, useSelector } from 'react-redux'
@@ -7,14 +7,56 @@ import NewsGrid from 'components/NewsGrid'
 import Container from '@material-ui/core/Container'
 // ACTIONS
 import { getAcceptedLatestNews } from 'app/slices/newsSlice'
+import { Pagination } from '@material-ui/lab'
+import { Box } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
+
+const useStyles = makeStyles({
+	paginationBox: {
+		marginTop: '32px',
+	},
+	pagination: {
+		'& .MuiPagination-ul': {
+			justifyContent: 'flex-end',
+		},
+	},
+})
 
 function HomePage() {
+	const classes = useStyles()
 	const dispatch = useDispatch()
 	const latestNews = useSelector(state => state.news.latestNews.data)
+
+	const [pagination, setPagination] = useState({
+		page: 1,
+		total: 0,
+		limit: 12,
+	})
+
+	const filterNews = useMemo(() => {
+		return latestNews.slice(
+			(pagination.page - 1) * pagination.limit,
+			pagination.page * pagination.limit
+		)
+	}, [latestNews, pagination])
 
 	useEffect(() => {
 		dispatch(getAcceptedLatestNews())
 	}, [dispatch])
+
+	useEffect(() => {
+		setPagination(prevPagination => ({
+			...prevPagination,
+			total: latestNews.length,
+		}))
+	}, [latestNews])
+
+	const handlePageChange = (event, newPage) => {
+		setPagination(prevPagination => ({
+			...prevPagination,
+			page: newPage,
+		}))
+	}
 
 	return (
 		<div className="home-page">
@@ -24,9 +66,22 @@ function HomePage() {
 						<h2 className="title">Các tin mới nhất</h2>
 						<div className="line"></div>
 					</div>
+
 					<div className="home-page__section-body">
-						<NewsGrid newsList={latestNews} />
+						<NewsGrid newsList={filterNews} />
 					</div>
+
+					<Box className={classes.paginationBox}>
+						<Pagination
+							page={pagination.page}
+							count={Math.ceil(pagination.total / pagination.limit)}
+							boundaryCount={1}
+							siblingCount={1}
+							size="medium"
+							onChange={handlePageChange}
+							className={classes.pagination}
+						/>
+					</Box>
 				</Container>
 			</section>
 		</div>
